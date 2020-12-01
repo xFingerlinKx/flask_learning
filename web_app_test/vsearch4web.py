2cb8814ba421d9bb26911a0f4c2382b79cc9873b
Artur Apanasov <aaa@admport.by>
2020-12-01 20:09:20 +0300

    [REF]redirecting + test_1.html

--- web_app_test/vsearch4web.py
+++ web_app_test/vsearch4web.py
@@ -1,24 +1,18 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+# from flask import redirect
 
 app = Flask(__name__)
 
 
-def search4vowels(phrase: str) -> set:
-    """ Returns the set of vowels found in phrase """
-    return set('aeiou').intersection(set(phrase))
-
-
-def search4letters(phrase: str, letters: str='aeiou') -> set:
-    """ Returns the set of letters found in phrase """
-    return set(letters).intersection(set(phrase))
-
-
-@app.route('/')
-def hello() -> str:
-    greeting_str = 'Hello world from Flask!'
-    return greeting_str
+# @app.route('/')
+# def redirect_to_entry():
+#     """
+#     Redirecting to '/entry' location.
+#     :return: code=302
+#     """
+#     return redirect(location='/entry')
 
 
 @app.route('/search4', methods=['GET', 'POST'])
@@ -42,6 +36,7 @@ def do_search():
     )
 
 
+@app.route('/')
 @app.route('/entry')
 def entry_page():
     """
@@ -52,6 +47,16 @@ def entry_page():
         template_name_or_list='entry.html',
         the_title='Welcome to search4letters on the web!',
     )
+
+
+def search4vowels(phrase: str) -> set:
+    """ Returns the set of vowels found in phrase """
+    return set('aeiou').intersection(set(phrase))
+
+
+def search4letters(phrase: str, letters: str='aeiou') -> set:
+    """ Returns the set of letters found in phrase """
+    return set(letters).intersection(set(phrase))
 
 
 if __name__ == '__main__':
