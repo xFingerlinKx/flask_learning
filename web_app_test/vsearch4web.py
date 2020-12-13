648c2e708e9af8d031348e446194ab013bfef5bd
Artur Apanasov <aaa@admport.by>
2020-12-13 14:56:31 +0300

    Learning Python book - Flask (p. 282-316)

--- web_app_test/vsearch4web.py
+++ web_app_test/vsearch4web.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import escape
 # from flask import redirect
 
 app = Flask(__name__)
@@ -13,6 +14,26 @@ app = Flask(__name__)
 #     :return: code=302
 #     """
 #     return redirect(location='/entry')
+def search4vowels(phrase: str) -> set:
+    """ Returns the set of vowels found in phrase """
+    return set('aeiou').intersection(set(phrase))
+
+
+def search4letters(phrase: str, letters: str='aeiou') -> set:
+    """ Returns the set of letters found in phrase """
+    return set(letters).intersection(set(phrase))
+
+
+def log_request(request, res: str) -> None:
+    """
+    Write request and the results in 'vsearch.log'.
+
+    :param request: request obj
+    :param res: result of request
+    :return: None
+    """
+    with open('vsearch.log', 'a') as log:
+        print(request.form, request.remote_addr, request.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['GET', 'POST'])
@@ -26,6 +47,8 @@ def do_search():
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+
+    log_request(request=request, res=results)
 
     return render_template(
         template_name_or_list='results.html',
@@ -49,14 +72,29 @@ def entry_page():
     )
 
 
-def search4vowels(phrase: str) -> set:
-    """ Returns the set of vowels found in phrase """
-    return set('aeiou').intersection(set(phrase))
+@app.route('/viewlog')
+def view_log_file() -> str:
+    """
+    Return the contents of the log file in HTML table.
 
+    :return: {HTML} contents of the log file.
+    """
+    contents = []
 
-def search4letters(phrase: str, letters: str='aeiou') -> set:
-    """ Returns the set of letters found in phrase """
-    return set(letters).intersection(set(phrase))
+    with open('vsearch.log') as log:
+        for line in log.readlines():
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+
+    titles = ('Form Data', 'Remote addr', 'User agent', 'Results')
+
+    return render_template(
+        template_name_or_list='log.html',
+        the_title='The log file results',
+        the_row_titles=titles,
+        the_data=contents,
+    )
 
 
 if __name__ == '__main__':
